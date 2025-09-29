672045495fc5b5fde69c2e54b984df70066f89ce
Felipe Oliveira <lipefan0@gmail.com>
2025-09-29 17:15:32 -0300

    initial commit

---
+++ src/main/kotlin/br/com/upvisibility/usuarioservice/UsuarioServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.upvisibility.usuarioservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsuarioServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<UsuarioServiceApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/upvisibility/usuarioservice/UsuarioServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.upvisibility.usuarioservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
