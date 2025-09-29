67f09cd0752e37bc266b0674d69c98ad31800dbc
Felipe Oliveira <lipefan0@gmail.com>
2025-09-29 17:31:32 -0300

    add UserEntity and UserRepository for user management

---
+++ src/main/kotlin/br/com/upvisibility/usuarioservice/entity/UserEntity.kt
@@ -0,0 +1,43 @@
+package br.com.upvisibility.usuarioservice.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "tb_users")
+data class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String? = null,
+
+    val name: String,
+
+    @Column(unique = true)
+    val email: String,
+
+    val password: String,
+
+    val createdAt: LocalDateTime,
+
+    val updatedAt: LocalDateTime
+): UserDetails {
+    override fun getAuthorities(): Collection<GrantedAuthority?>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPassword(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUsername(): String? {
+        TODO("Not yet implemented")
+    }
+
+}
